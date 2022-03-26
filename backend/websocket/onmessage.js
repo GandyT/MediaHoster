32ec6e944e8547fb57e561a6ba47934a5afb0c38
Randy Sim <randysim9@gmail.com>
2022-03-26 15:58:40 -0400

    onmessage stuff

--- backend/websocket/onmessage.js
+++ backend/websocket/onmessage.js
@@ -1,12 +1,78 @@
+const RoomManager = require("./websocket/roomManager.js");
+const ErrorLoad = JSON.stringify({ success: false });
+const SuccessLoad = JSON.stringify({success: true});
+
+// note: idk if the way im referencing stuff would work cuz i didn't do this for a while
+// some1 check and then delete this comment later
+
 function onmessage(payload) {
     const socket = this;
-    const playerId = socket.id;
+    const inRoom = false;
     var data = JSON.parse(payload);
     var op = data.op;
+    var d = data.d; // main data stored here ( sorry confusing variable names xd )
 
    if (op == 1) {
        // create room
+        if (inRoom) {
+            socket.send(ErrorLoad)
+            return;
+        }
+
+       let roomCode = RoomManager.createRoom();
        
+       socket.send(JSON.stringify({ success: true, op: 2, t: "ROOM_CREATED", data: { code: roomCode }}));
+   } else if (op == 2) {
+        // join room 
+        if (inRoom) return socket.send(ErrorLoad)
+
+        let username = d.username
+        let roomCode = d.code
+        let room = RoomManager.getRoom(roomCode)
+
+        if (!room) return socket.send(ErrorLoad);
+
+        room.addPlayer(socket, username);
+
+        socket.inRoom = true;
+        socket.on("close", () => room.removePlayer(socket));
+        socket.send(JSON.stringify({ success: true, t: "ROOM_JOIN", op: 6, d: {
+            players: room.players,
+            videoTime: room.videoTime,
+            paused: room.paused
+        }}))
+   } else if (op == 3) {
+        // leave room
+        if (!inRoom) return socket.send(ErrorLoad)
+
+        let roomCode = d.code;
+        let room = RoomManager.getRoom(roomCode);
+
+        if (!room) return socket.send(ErrorLoad);
+
+        room.removePlayer(socket);
+
+        if (room.players.length <= 0) {
+            RoomManager.removeRoom(roomCode);
+        }
+
+        socket.inRoom = false;
+
+        socket.send(SuccessLoad);
+   } else if (op == 4) {
+       // update room
+       if (!inRoom) return socket.send(ErrorLoad);
+
+       let roomCode = d.code;
+       let room = RoomManager.getRoom(roomCode);
+
+       if (!room) return socket.send(ErrorLoad);
+
+       for (let [key, value] of Object.entries(d)) {
+           room[key] = value;
+       }
+
+       socket.send(SuccessLoad);
    }
 }
 
